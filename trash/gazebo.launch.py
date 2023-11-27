793071eb1c599da4aa1a5c03414cbc69382dc9d9
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-11-27 14:59:20 -0600

    UODATDED

--- trash/gazebo.launch.py
+++ trash/gazebo.launch.py
@@ -1,15 +1,22 @@
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
+#!/usr/bin/env python3
+
 import os
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+import launch_ros
+
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='sdv_description').find('sdv_description')
-    
-    default_model_path = os.path.join(pkg_share, 'urdf/robot.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_con.rviz')
     world_path=os.path.join(pkg_share, 'world/empty_world.sdf')
+    
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    urdf_file_name = 'robot.urdf'
     use_sim_time = LaunchConfiguration('use_sim_time') 
 
     # Position and orientation
@@ -18,19 +25,34 @@ def generate_launch_description():
     # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
     
+    print('urdf_file_name : {}'.format(urdf_file_name))
+
+    urdf = os.path.join(
+        get_package_share_directory('sdv_description'),
+        'urdf',
+        urdf_file_name)
+
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         name='use_sim_time',
         default_value='true',
         description='Use simulation (Gazebo) clock if true'
         )
-
+    
+    
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        emulate_tty=True,
-        parameters=[{'use_sim_time': use_sim_time, 'robot_description': Command(['xacro ', LaunchConfiguration('model')])}],
-        arguments=[default_model_path],
-    )
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+                        'use_sim_time': use_sim_time,
+                        'robot_description': Command(['xacro ', LaunchConfiguration('model')]),
+                    }],
+        arguments=[urdf])
     
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
@@ -38,25 +60,25 @@ def generate_launch_description():
         name='joint_state_publisher',
         parameters=[{'use_sim_time': use_sim_time}],
     )
+    
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        # output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
         parameters=[{'use_sim_time': use_sim_time}],
     )
+    
     spawn_entity = launch_ros.actions.Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-entity', 'sdv_car', '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]), '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),'-topic', '/robot_description'],
+        arguments=['-entity', 'dasautonomeauto', '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]), '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),'-topic', '/robot_description'],
         output='screen'
     )
     
-            
     return launch.LaunchDescription([
-        
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+
+        launch.actions.DeclareLaunchArgument(name='model', default_value=urdf,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
@@ -66,10 +88,8 @@ def generate_launch_description():
         robot_state_publisher_node,
         TimerAction(
             actions=[
-                
-                spawn_entity,
                 rviz_node,
-
+                spawn_entity
             ],
             period='2.0',  
         ),
